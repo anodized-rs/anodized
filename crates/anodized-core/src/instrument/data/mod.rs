129e9ac64901703622a4db7873914eef189861b6
Mate Kovacs <mkovaxx@gmail.com>
2026-05-01 00:28:49 +0200

    get it working

--- crates/anodized-core/src/instrument/data/mod.rs
+++ crates/anodized-core/src/instrument/data/mod.rs
@@ -1,12 +1,7 @@
 #[cfg(test)]
 mod tests;
 
-use proc_macro2::Span;
-use syn::{
-    AngleBracketedGenericArguments, Attribute, GenericArgument, GenericParam, Generics, Ident,
-    ImplItem, ImplItemFn, ItemImpl, ReturnType, Signature, Type, parse_quote,
-    punctuated::Punctuated,
-};
+use syn::{Generics, Ident, ItemImpl, parse_quote};
 
 use crate::{DataSpec, instrument::Backend};
 
@@ -23,83 +18,13 @@ impl Backend {
             body.stmts.insert(0, parse_quote!(use #ident::*;));
         }
 
-        let spec_maintains_fn = ImplItemFn {
-            attrs: vec![],
-            vis: syn::Visibility::Inherited,
-            sig: Self::build_type_spec_fn_sig("__anodized_data_maintains"),
-            block: body,
-            defaultness: None,
-        };
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-        let attrs: [Attribute; 2] = [
-            parse_quote!(#[doc(hidden)]),
-            parse_quote!(#[allow(warnings)]),
-        ];
-
-        let generic_args = Self::build_generic_args_from_params(generics);
-        let self_type: Type = parse_quote!(#ident #generic_args);
-        ItemImpl {
-            attrs: attrs.to_vec(),
-            defaultness: None,
-            unsafety: None,
-            impl_token: Default::default(),
-            generics: generics.clone(),
-            trait_: None,
-            self_ty: Box::new(self_type),
-            brace_token: Default::default(),
-            items: vec![ImplItem::Fn(spec_maintains_fn)],
-        }
-    }
-
-    pub fn build_type_spec_fn_sig(name: &str) -> Signature {
-        Signature {
-            constness: None,
-            asyncness: None,
-            unsafety: None,
-            abi: None,
-            fn_token: Default::default(),
-            ident: Ident::new(name, Span::mixed_site()),
-            generics: Default::default(),
-            paren_token: Default::default(),
-            inputs: parse_quote!(&self),
-            variadic: None,
-            output: ReturnType::Default,
-        }
-    }
-
-    pub fn build_generic_args_from_params(
-        params: &Generics,
-    ) -> Option<AngleBracketedGenericArguments> {
-        let mut args = Punctuated::new();
-
-        for pair in params.params.pairs() {
-            let arg = Self::build_generic_arg_from_param(pair.value());
-            args.push_value(arg);
-            if let Some(punct) = pair.punct() {
-                args.push_punct(**punct);
-            }
-        }
-
-        Some(AngleBracketedGenericArguments {
-            colon2_token: None,
-            lt_token: params.lt_token?,
-            args,
-            gt_token: params.gt_token?,
-        })
-    }
-
-    pub fn build_generic_arg_from_param(param: &GenericParam) -> GenericArgument {
-        match param {
-            GenericParam::Lifetime(lifetime_param) => {
-                GenericArgument::Lifetime(lifetime_param.lifetime.clone())
-            }
-            GenericParam::Type(type_param) => {
-                let ident = &type_param.ident;
-                GenericArgument::Type(parse_quote!(#ident))
-            }
-            GenericParam::Const(const_param) => {
-                let ident = &const_param.ident;
-                GenericArgument::Const(parse_quote!(#ident))
+        parse_quote! {
+            #[doc(hidden)]
+            #[allow(warnings)]
+            impl #impl_generics #ident #ty_generics #where_clause {
+                fn __anodized_data_maintains(&self) #body
             }
         }
     }
