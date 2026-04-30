91c021e47b2086797fc395c06d8f337927dfc661
Mate Kovacs <mkovaxx@gmail.com>
2026-04-30 16:33:32 +0200

    only check sortedness of known spec params

--- crates/anodized-core/src/annotate/mod.rs
+++ crates/anodized-core/src/annotate/mod.rs
@@ -27,7 +27,11 @@ impl Parse for Spec {
         let mut binds_pattern: Option<Pat> = None;
         let mut ensures: Vec<PostCondition> = vec![];
 
-        let is_sorted = raw_spec.args.iter().is_sorted_by_key(|arg| &arg.keyword);
+        let is_sorted = raw_spec
+            .args
+            .iter()
+            .filter(|arg| !matches!(&arg.keyword, Keyword::Unknown(_)))
+            .is_sorted_by_key(|arg| &arg.keyword);
 
         for arg in raw_spec.args {
             match &arg.keyword {
