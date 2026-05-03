1d1a8bc7113faf52e0910ee48a37bbbbbe864383
Mate Kovacs <mkovaxx@gmail.com>
2026-05-04 00:51:06 +0200

    add new test

--- crates/anodized-core/src/instrument/traits/tests.rs
+++ crates/anodized-core/src/instrument/traits/tests.rs
@@ -2,7 +2,7 @@ use crate::test_util::assert_tokens_eq;
 
 use super::*;
 use proc_macro2::TokenStream;
-use syn::{ItemTrait, parse_quote};
+use syn::{ItemImpl, ItemTrait, parse_quote};
 
 #[test]
 fn embed_spec_item_trait() {
@@ -55,6 +55,57 @@ fn embed_spec_item_trait() {
 
     let observed = Backend::NOTHING
         .instrument_item_trait(trait_spec, item_trait)
+        .unwrap();
+    assert_tokens_eq(&observed, &expected);
+}
+
+#[test]
+fn embed_spec_item_impl_trait() {
+    let trait_spec = DataSpec::empty();
+    let item_impl: ItemImpl = parse_quote! {
+        impl TRAIT for IMPL_TYPE {
+            #[spec(
+                requires: COND_1,
+                maintains: COND_2,
+                binds: PAT_1,
+                ensures: COND_3,
+            )]
+            fn FUNC(&self, PARAM_1: TYPE_1, PARAM_2: TYPE_2) -> RET_TYPE {
+                BODY
+            }
+        }
+    };
+
+    let expected: TokenStream = parse_quote! {
+        impl TRAIT for IMPL_TYPE {
+            #[doc(hidden)]
+            fn __anodized_FUNC(&self, PARAM_1: TYPE_1, PARAM_2: TYPE_2) -> RET_TYPE {
+                BODY
+            }
+
+            #[doc(hidden)]
+            #[allow(warnings)]
+            fn __anodized_fn_requires_FUNC(&self, PARAM_1: TYPE_1, PARAM_2: TYPE_2) {
+                let _ = | | COND_1;
+            }
+
+            #[doc(hidden)]
+            #[allow(warnings)]
+            fn __anodized_fn_maintains_FUNC(&self, PARAM_1: TYPE_1, PARAM_2: TYPE_2) {
+                let _ = | | COND_2;
+            }
+
+            #[doc(hidden)]
+            #[allow(warnings)]
+            fn __anodized_fn_ensures_FUNC(&self, PARAM_1: TYPE_1, PARAM_2: TYPE_2) {
+                let () = ();
+                let _ = |PAT_1: &RET_TYPE| COND_3;
+            }
+        }
+    };
+
+    let observed = Backend::NOTHING
+        .instrument_item_trait_impl(trait_spec, item_impl)
         .unwrap();
     assert_tokens_eq(&observed, &expected);
 }
