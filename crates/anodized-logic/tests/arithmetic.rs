e2bd093c62945550fe9e3546fba375015716ba63
Mate Kovacs <mkovaxx@gmail.com>
2026-05-11 18:09:31 +0200

    improve tests

--- crates/anodized-logic/tests/arithmetic.rs
+++ crates/anodized-logic/tests/arithmetic.rs
@@ -1,12 +1,12 @@
 use anodized_logic::arithmetic::int;
 
-fn f(n: int) -> int {
-    if n % 2 == 0 { n / 2 } else { 3 * n + 1 }
-}
-
-fn collatz(mut n: int) -> int {
+pub fn collatz(mut n: int) -> int {
     while n > 1 {
         n = f(n);
     }
     n
+}
+
+fn f(n: int) -> int {
+    if n % 2 == 0 { n / 2 } else { 3 * n + 1 }
 }
