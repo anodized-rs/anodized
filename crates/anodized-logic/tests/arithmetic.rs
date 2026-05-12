371441be908a5935b928b85e46b039c51010c93b
Mate Kovacs <mkovaxx@gmail.com>
2026-05-12 17:40:08 +0200

    clippy

--- crates/anodized-logic/tests/arithmetic.rs
+++ crates/anodized-logic/tests/arithmetic.rs
@@ -5,8 +5,8 @@ fn test_basics() {
     assert_eq!(int::from(1) + 1, 2);
     assert_eq!(1 + int::from(1), 2);
 
-    let large_num = int::from(std::u128::MAX) + 42;
-    assert_eq!(large_num - 42, std::u128::MAX);
+    let large_num = int::from(u128::MAX) + 42;
+    assert_eq!(large_num - 42, u128::MAX);
 }
 
 #[test]
@@ -22,6 +22,6 @@ fn test_collatz() {
         if n % 2 == 0 { n / 2 } else { 3 * n + 1 }
     }
 
-    let large_num = int::from(std::u128::MAX) + 42;
+    let large_num = int::from(u128::MAX) + 42;
     assert_eq!(collatz(large_num), 1);
 }
