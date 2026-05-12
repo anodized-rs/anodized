c9a72c487441b00d8d9d6051051fe69d03032d8f
Máté Kovács <481354+mkovaxx@users.noreply.github.com>
2026-05-12 17:44:42 +0200

    [anodized-logic] feat: `int` type to embed the integers (#127)
    - Add the `arithmetic` module and the `int` type.
    - The `int` type opaquely wraps `BigInt` from the `num-bigint` crate.
    - Ensure interoperability for `int` with primitive Rust integral types.
    - Add unit and integration tests.

---
+++ crates/anodized-logic/tests/arithmetic.rs
@@ -0,0 +1,27 @@
+use anodized_logic::arithmetic::int;
+
+#[test]
+fn test_basics() {
+    assert_eq!(int::from(1) + 1, 2);
+    assert_eq!(1 + int::from(1), 2);
+
+    let large_num = int::from(u128::MAX) + 42;
+    assert_eq!(large_num - 42, u128::MAX);
+}
+
+#[test]
+fn test_collatz() {
+    pub fn collatz(mut n: int) -> int {
+        while n > 1 {
+            n = f(&n);
+        }
+        n
+    }
+
+    fn f(n: &int) -> int {
+        if n % 2 == 0 { n / 2 } else { 3 * n + 1 }
+    }
+
+    let large_num = int::from(u128::MAX) + 42;
+    assert_eq!(collatz(large_num), 1);
+}
