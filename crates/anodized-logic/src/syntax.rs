368d873b3e3a6c58c26db6927c73118d1211d99e
Mate Kovacs <mkovaxx@gmail.com>
2026-05-20 12:14:24 +0900

    add comment

--- crates/anodized-logic/src/syntax.rs
+++ crates/anodized-logic/src/syntax.rs
@@ -1,5 +1,9 @@
 pub use crate::opaque;
 
+/// An opaque expression.
+///
+/// Useful to pass through a term that cannot be rendered as a Rust expression.
+/// The semantics of an opaque expression is defined only by the interpreting backend.
 #[macro_export]
 macro_rules! opaque {
     ($($t:tt)*) => {{
