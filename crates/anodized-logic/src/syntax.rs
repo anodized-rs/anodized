7fee51848e1b8a6e020990ac545296a134a5b66a
Mate Kovacs <mkovaxx@gmail.com>
2026-05-20 12:55:39 +0900

    rework

--- crates/anodized-logic/src/syntax.rs
+++ crates/anodized-logic/src/syntax.rs
@@ -1,15 +1,19 @@
 pub use crate::opaque;
 
-/// An opaque expression.
+/// Embed Rust syntax as an opaque expression.
 ///
-/// Useful to pass through a term that cannot be rendered as a Rust expression.
+/// Useful to pass through a term that cannot be rendered as a valid Rust expression.
 /// The semantics of an opaque expression is defined only by the interpreting backend.
 #[macro_export]
 macro_rules! opaque {
-    ($($t:tt)*) => {{
-        #[allow(clippy::diverging_sub_expression)]
-        let bottom: bool = panic!("Cannot run `opaque!` expression: {}", stringify!($($t)*));
-        #[allow(unreachable_code)]
-        bottom
-    }}
+    ($($t:tt)*) => {
+        $crate::syntax::opaque_str(stringify!($($t)*))
+    }
+}
+
+/// Embed a string as an opaque expression.
+///
+/// You probably want the [`opaque!`] macro instead.
+pub fn opaque_str(expr: &str) -> bool {
+    panic!("Cannot run `opaque!` expression: {}", expr)
 }
