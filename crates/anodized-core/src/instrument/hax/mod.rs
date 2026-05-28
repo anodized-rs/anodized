ad21353078f9dd41054216b95a43f0c79f8f3bf9
Mate Kovacs <mkovaxx@gmail.com>
2026-05-28 19:26:03 +0900

    Haxify loops

--- crates/anodized-core/src/instrument/hax/mod.rs
+++ crates/anodized-core/src/instrument/hax/mod.rs
@@ -1,6 +1,6 @@
-use syn::{Attribute, Expr, parse_quote};
+use syn::{Attribute, Expr, Pat, Stmt, parse_quote};
 
-use crate::Spec;
+use crate::{LoopSpec, Spec};
 
 pub(crate) fn haxify_fn(spec: &Spec, attrs: &mut Vec<Attribute>) {
     for precond in &spec.requires {
@@ -21,6 +21,24 @@ pub(crate) fn haxify_fn(spec: &Spec, attrs: &mut Vec<Attribute>) {
 
 pub(crate) fn haxify_impl_or_trait(attrs: &mut Vec<Attribute>) {
     attrs.push(parse_quote! { #[::hax_lib::attributes] });
+}
+
+pub(crate) fn haxify_for_loop(spec: &LoopSpec, pat: &Pat, stmts: &mut Vec<Stmt>) {
+    for condition in &spec.maintains {
+        let invariant = haxify_expr(&condition.closure.body);
+        stmts.push(parse_quote! { ::hax_lib::loop_invariant!(|#pat| #invariant); });
+    }
+}
+
+pub(crate) fn haxify_while_loop(spec: &LoopSpec, stmts: &mut Vec<Stmt>) {
+    for condition in &spec.maintains {
+        let invariant = haxify_expr(&condition.closure.body);
+        stmts.push(parse_quote! { ::hax_lib::loop_invariant!(#invariant); });
+    }
+    if let Some(expr) = &spec.decreases {
+        let variant = haxify_expr(&expr.expr);
+        stmts.push(parse_quote! { ::hax_lib::loop_decreases!(#variant); });
+    }
 }
 
 pub(crate) fn haxify_expr(expr: &Expr) -> Expr {
