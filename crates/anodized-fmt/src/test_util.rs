99261725671c199c2643bd6d31ffc6787437de6d
Mate Kovacs <mkovaxx@gmail.com>
2026-06-10 18:59:34 +0900

    refine plan

--- crates/anodized-fmt/src/test_util.rs
+++ crates/anodized-fmt/src/test_util.rs
@@ -1,7 +1,7 @@
 //! This module contains utilities meant only for testing.
 //! Do not use outside of tests.
 
-/// Inserts random comments into a fragment of well-formed Rust code.
+/// Inserts random comments into a fragment of tokenizable Rust code.
 ///
 /// Valid positions to insert a comment are:
 /// - inside whitespace
@@ -10,7 +10,7 @@ pub fn inject_comments(source: &str) -> String {
     todo!()
 }
 
-/// Checks that each comment is indented correctly in a fragment of well-formed Rust code.
+/// Checks that each comment is indented correctly in a fragment of tokenizable Rust code.
 ///
 /// A comment is indented correctly in any of the following cases:
 /// - The following token starts on the same line.
@@ -21,12 +21,12 @@ pub fn is_comment_indentation_valid(source: &str) -> bool {
     todo!()
 }
 
-/// Breaks a fragment of well-formed Rust code into a sequence of elements.
-pub fn break_into_elems(source: &str) -> Vec<Elem<&str>> {
+/// Breaks a fragment of well-formed Rust code into a sequence of tokens.
+pub fn break_into_tokens(source: &str) -> Vec<Token<&str>> {
     todo!()
 }
 
-pub struct Elem<Text> {
+pub struct Token<Text> {
     pub text: Text,
     pub tag: Tag,
 }
@@ -34,13 +34,15 @@ pub struct Elem<Text> {
 pub enum Tag {
     /// A single line break (either "\n" or "\r\n").
     Linebreak,
-    /// A span of whitespace characters, without line breaks.
+    /// A span of whitespace characters, with no line breaks.
     Whitespace,
     /// A line (doc)comment, including its terminating line break.
     LineComment,
     /// A block (doc)comment.
     BlockComment,
-    /// A span of punctuation characters.
+    /// A span of glued punctuation characters.
+    ///
+    /// Invariant: `(0..text.len()).all(|i| is_punct_pair_glued(&text[i..i+1]))`
     Punct,
     /// Other things: identifiers, literals, etc.
     Other,
@@ -48,9 +50,12 @@ pub enum Tag {
 
 /// Returns whether an (ordered) pair of characters is "glued".
 ///
-/// The sequence `AB` is glued iff both characters are punctuation, and there's a valid Rust program in which `AB` appears as a punctuation sequence (e.g. inside an operator), and inserting a space between them (`A<space>B` makes the program invalid.
-pub fn is_punct_pair_glued(a: char, b: char) -> bool {
-    todo!()
+/// The pair is glued iff both characters are punctuation, and there's a valid Rust program in which `AB` appears as a punctuation sequence (e.g. inside an operator), and inserting a space between them (i.e. `A<space>B`) makes the program invalid.
+pub fn is_punct_pair_glued(ab: &str) -> bool {
+    ab.len() == 2 && GLUED_PAIRS.contains(&ab)
 }
 
-const GLUED_PAIRS: [&'static str; 8] = ["==", "!=", "<=", ">=", "->", "=>", "<<", ">>"];
+const GLUED_PAIRS: [&str; 21] = [
+    "==", "!=", "<=", ">=", "+=", "-=", "*=", "/=", "%=", "^=", "&=", "|=", "&&", "||", "<<", ">>",
+    "::", "..", "->", "=>", ".=",
+];
