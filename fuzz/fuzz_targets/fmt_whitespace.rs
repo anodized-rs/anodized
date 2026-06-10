cf9b892ec1560a6a83c4eb10626c110335b1f9e6
Mate Kovacs <mkovaxx@gmail.com>
2026-06-11 02:04:34 +0900

    make it kinda work

---
+++ fuzz/fuzz_targets/fmt_whitespace.rs
@@ -0,0 +1,56 @@
+#![no_main]
+
+use std::sync::OnceLock;
+
+use anodized_fmt::{Config, format_file};
+use libfuzzer_sys::fuzz_target;
+use test_util::fmt::{Template, Variant};
+
+static TEMPLATE: OnceLock<Template> = OnceLock::new();
+
+/// Build a template for the following fragment:
+///
+///     #[spec(
+///         // the precondition
+///         requires: x > 0,
+///         // the postcondition
+///         ensures: *output > 0,
+///     )]
+///     fn func(x: i32) -> i32 {
+///         todo!()
+///     }
+///
+#[rustfmt::skip]
+fn make_template() -> Template {
+    Template::new()
+        .z().code("# [ spec (")
+        .z().line_comment(" the precondition")
+        .z().code("requires : x > 0 ,")
+        .z().line_comment(" the postcondition")
+        .z().code("ensures : * output > 0 ,")
+        .z().code(") ]")
+        .z().code("fn").p().code("func ( x : i32 ) -> i32 {")
+        .z().code("todo ! ( )")
+        .z().code("}")
+        .z()
+}
+
+fuzz_target!(
+    init: {
+        TEMPLATE.set(make_template()).unwrap();
+    },
+    |variant: Variant| {
+        let config = Config::default();
+
+        let template = TEMPLATE.get().unwrap();
+        let default_input = template.generate(Variant::default());
+        dbg!(&default_input);
+        let variant_input = template.generate(variant);
+        dbg!(&variant_input);
+
+        let fmt_default = format_file(&default_input, &config).expect("formatting default");
+        let fmt_variant = format_file(&variant_input, &config).expect("formatting variant");
+
+        assert_eq!(fmt_variant, fmt_default);
+    }
+);
