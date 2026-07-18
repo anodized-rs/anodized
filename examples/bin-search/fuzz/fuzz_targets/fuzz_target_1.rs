f525ee00b917636994f9fa17d42b622c6aba1473
Mate Kovacs <mkovaxx@gmail.com>
2026-07-18 20:58:30 +0900

    extend example to generate inputs more efficiently

--- examples/bin-search/fuzz/fuzz_targets/fuzz_target_1.rs
+++ examples/bin-search/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,16 +1,40 @@
 #![no_main]
 
-use libfuzzer_sys::arbitrary::{Arbitrary, Unstructured};
+use libfuzzer_sys::arbitrary::{Arbitrary, Error, Unstructured};
 use libfuzzer_sys::{fuzz_target, Corpus};
 
 fuzz_target!(|data: &[u8]| -> Corpus {
     let mut unst = Unstructured::new(data);
-    let Ok((in0, in1)) = <(Vec<i32>, i32) as Arbitrary>::arbitrary(&mut unst) else {
+    let Ok((seq, value)) = <(AscendingVec<i32>, i32) as Arbitrary>::arbitrary(&mut unst) else {
         return Corpus::Reject;
     };
-    match ::bin_search::__anodized_fn_split_bin_search(&in0, &in1) {
+    match ::bin_search::__anodized_fn_split_bin_search(&seq, &value) {
         Ok(_) => Corpus::Keep,
         Err((false, _)) => Corpus::Reject,
-        Err((true, errors)) => panic!("postcondition failed:{errors}"),
+        Err((true, errors)) => {
+            eprintln!("inputs:");
+            dbg!(seq);
+            dbg!(value);
+            panic!("postcondition failed:{errors}");
+        }
     }
 });
+
+/// Helper newtype to more efficiently generate valid inputs.
+#[derive(Debug, Clone)]
+struct AscendingVec<T>(Vec<T>);
+
+impl<'a, T: Ord + Arbitrary<'a>> Arbitrary<'a> for AscendingVec<T> {
+    fn arbitrary(unst: &mut Unstructured<'a>) -> Result<Self, Error> {
+        let mut raw_seq = <Vec<T> as Arbitrary>::arbitrary(unst)?;
+        raw_seq.sort();
+        Ok(Self(raw_seq))
+    }
+}
+
+impl<T> std::ops::Deref for AscendingVec<T> {
+    type Target = Vec<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
