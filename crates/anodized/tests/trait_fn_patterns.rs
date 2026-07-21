d3e11bcc98aba28561fc93b3915dbf18619f2c6e
Mate Kovacs <mkovaxx@gmail.com>
2026-07-21 15:39:44 +0900

    improve tests

--- crates/anodized/tests/trait_fn_patterns.rs
+++ crates/anodized/tests/trait_fn_patterns.rs
@@ -3,22 +3,66 @@
 use anodized::spec;
 
 #[spec]
-trait TraitA {
-    #[spec(requires: left <= right)]
-    fn func(input @ (left, right): (i32, i32)) {
-        let _ = (left, right);
+trait Trait {
+    #[spec(requires: left < right)]
+    fn func_1(input @ (left, right): (i32, i32)) {
+        unimplemented!()
+    }
+
+    #[spec(requires: lower < upper)]
+    fn func_2(Bounds { lower, upper }: Bounds) {
+        unimplemented!()
+    }
+
+    #[spec(
+        requires: [
+            lower < upper,
+            i >= i,
+            i < upper,
+        ],
+    )]
+    fn func_3((i, Bounds { lower, upper }): (i32, Bounds)) {
+        unimplemented!()
     }
 }
 
-pub struct Bounds {
+struct Bounds {
     lower: i32,
     upper: i32,
 }
 
+struct Type;
+
 #[spec]
-trait TraitB {
-    #[spec(requires: lower <= upper)]
-    fn func(Bounds { lower, upper }: Bounds) {
-        let _ = (lower, upper);
-    }
+impl Trait for Type {
+    fn func_1(_: (i32, i32)) {}
+    fn func_2(_: Bounds) {}
+    fn func_3(_: (i32, Bounds)) {}
+}
+
+#[cfg(all(anodized_print, anodized_panic))]
+#[test]
+#[should_panic(expected = r#"precondition failed:
+    left < right"#)]
+fn test_1() {
+    let _ = Type::func_1((42, 1));
+}
+
+#[cfg(all(anodized_print, anodized_panic))]
+#[test]
+#[should_panic(expected = r#"precondition failed:
+    lower < upper"#)]
+fn test_2() {
+    let _ = Type::func_2(Bounds {
+        lower: 42,
+        upper: 1,
+    });
+}
+
+#[cfg(all(anodized_print, anodized_panic))]
+#[test]
+#[should_panic(expected = r#"precondition failed:
+    i < upper"#)]
+fn test_3() {
+    let _ = Type::func_3((42, Bounds { lower: 1, upper: 5 }));
 }
