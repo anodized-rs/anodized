978ae7992a5dec5882bd3aee5e2a782bdc39f0fb
Mate Kovacs <mkovaxx@gmail.com>
2026-07-21 16:01:15 +0900

    refine test

--- crates/anodized/tests/trait_fn_patterns.rs
+++ crates/anodized/tests/trait_fn_patterns.rs
@@ -3,41 +3,44 @@
 use anodized::spec;
 
 #[spec]
-trait Trait {
+trait Trait<T: Ord + Copy> {
     #[spec(requires: left < right)]
     fn func_1(input @ (left, right): (i32, i32)) {
         unimplemented!()
     }
 
     #[spec(requires: lower < upper)]
-    fn func_2(Bounds { lower, upper }: Bounds) {
+    fn func_2(Bounds { lower, upper }: Bounds<T>) {
         unimplemented!()
     }
 
     #[spec(
         requires: lower < upper,
+        // NOTE: `T: Copy` is needed for this to work at the moment.
         ensures: [
             *output >= lower,
             *output < upper,
         ],
     )]
-    fn func_3((i, Bounds { lower, upper }): (i32, Bounds)) -> i32 {
+    fn func_3((input, Bounds { lower, upper }): (T, Bounds<T>)) -> T {
         unimplemented!()
     }
 }
 
-struct Bounds {
-    lower: i32,
-    upper: i32,
+struct Bounds<T> {
+    lower: T,
+    upper: T,
 }
 
-struct Type;
+struct Type<T> {
+    _phantom: std::marker::PhantomData<T>,
+}
 
 #[spec]
-impl Trait for Type {
+impl<T: Ord + Copy> Trait<T> for Type<T> {
     fn func_1(_: (i32, i32)) {}
-    fn func_2(_: Bounds) {}
-    fn func_3((i, _): (i32, Bounds)) -> i32 {
+    fn func_2(_: Bounds<T>) {}
+    fn func_3((i, _): (T, Bounds<T>)) -> T {
         i
     }
 }
@@ -47,7 +50,7 @@ impl Trait for Type {
 #[should_panic(expected = r#"precondition failed:
     left < right"#)]
 fn test_1() {
-    let _ = Type::func_1((42, 1));
+    let _ = Type::<i32>::func_1((42, 1));
 }
 
 #[cfg(all(anodized_print, anodized_panic))]
