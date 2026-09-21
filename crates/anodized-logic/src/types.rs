c571c7abd889d9b45de4aba83fcb17b3f89c931d
Mate Kovacs <mkovaxx@gmail.com>
2026-09-22 00:16:32 +0900

    add Spec trait

---
+++ crates/anodized-logic/src/types.rs
@@ -0,0 +1,111 @@
+#[diagnostic::on_unimplemented(
+    label = "type at the boundary of a `#[spec]`",
+    message = "\
+type spec enforcement needs `{Self}` to implement trait `anodized::types::Spec`",
+    note = "\
+if `{Self}` is a concrete local type, place a `#[spec]` attribute on its definition",
+    note = "\
+if `{Self}` is a concrete foreign type, wrap it in a local type such as `struct NewType({Self})`",
+    note = "\
+if `{Self}` is a type parameter, restrict it with the trait `{Self}: Spec`",
+    note = "\
+*UNSAFE*: alternatively, use `#[uncheck]` to locally disable type spec enforcement here"
+)]
+/// Defines a type refinement.
+pub trait Spec {
+    /// Returns `true` for a valid value and `false` otherwise.
+    fn predicate(&self) -> bool;
+}
+
+impl<T: Spec + ?Sized> Spec for &T {
+    fn predicate(&self) -> bool {
+        <T as Spec>::predicate(self)
+    }
+}
+
+impl<T: Spec + ?Sized> Spec for &mut T {
+    fn predicate(&self) -> bool {
+        <T as Spec>::predicate(self)
+    }
+}
+
+impl<T: Spec> Spec for [T] {
+    fn predicate(&self) -> bool {
+        self.iter().all(<T as Spec>::predicate)
+    }
+}
+
+impl<T: Spec> Spec for Option<T> {
+    fn predicate(&self) -> bool {
+        match self {
+            Some(inner) => inner.predicate(),
+            None => true,
+        }
+    }
+}
+
+impl<T: Spec, E: Spec> Spec for Result<T, E> {
+    fn predicate(&self) -> bool {
+        match self {
+            Ok(okay) => okay.predicate(),
+            Err(err) => err.predicate(),
+        }
+    }
+}
+
+impl<T: Spec + ?Sized> Spec for Box<T> {
+    fn predicate(&self) -> bool {
+        self.as_ref().predicate()
+    }
+}
+
+impl<T: Spec> Spec for Vec<T> {
+    fn predicate(&self) -> bool {
+        <[T] as Spec>::predicate(self.as_slice())
+    }
+}
+
+impl Spec for () {
+    fn predicate(&self) -> bool {
+        true
+    }
+}
+
+impl<T1: Spec + ?Sized> Spec for (T1,) {
+    fn predicate(&self) -> bool {
+        self.0.predicate()
+    }
+}
+
+impl<T1: Spec, T2: Spec + ?Sized> Spec for (T1, T2) {
+    fn predicate(&self) -> bool {
+        self.0.predicate() && self.1.predicate()
+    }
+}
+
+impl<T1: Spec, T2: Spec, T3: Spec + ?Sized> Spec for (T1, T2, T3) {
+    fn predicate(&self) -> bool {
+        self.0.predicate() && self.1.predicate() && self.2.predicate()
+    }
+}
+
+/// Implement `Spec` for concrete types, with `predicate` always `true`.
+#[macro_export]
+macro_rules! trivial_refinement {
+    ($($ty:ty),+ $(,)?) => {
+        $(
+            impl $crate::types::Spec for $ty {
+                fn predicate(&self) -> bool { true }
+            }
+        )+
+    };
+}
+
+#[rustfmt::skip]
+trivial_refinement!(
+    bool,
+    u8, u16, u32, u64, u128, usize,
+    i8, i16, i32, i64, i128, isize,
+    f32, f64,
+    char, str, String,
+);
--- crates/anodized/src/lib.rs
+++ crates/anodized/src/lib.rs
@@ -1,7 +1,7 @@
 #![doc = include_str!("../README.md")]
 
 pub use anodized_logic as logic;
-pub use anodized_logic::arithmetic;
+pub use anodized_logic::{arithmetic, types};
 pub use anodized_macros::{spec, unspec};
 
 pub mod __;
