df1bb05e69e1f5d6e40d2e712b7880bdc096c1e0
Mate Kovacs <mkovaxx@gmail.com>
2026-09-26 21:15:21 +0900

    factor out commands

---
+++ crates/cargo-anodized/src/commands/fmt.rs
@@ -0,0 +1,46 @@
+use std::{
+    collections::BTreeSet as Set,
+    path::PathBuf,
+    process::{Command, ExitStatus},
+};
+
+type Result<T> = std::result::Result<T, Error>;
+
+pub fn fmt() -> Result<()> {
+    let output = Command::new("cargo")
+        .args(["fmt", "--", "--verbose"])
+        .output()?;
+
+    let paths = formatted_paths(&output.stdout)?;
+    for path in paths {
+        println!("{}", path.display());
+    }
+
+    if !output.status.success() {
+        return Err(Error::CargoFmtFailed(output.status));
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("failed to run `cargo fmt`: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("`cargo fmt` output was not valid UTF-8: {0}")]
+    Utf8(#[from] std::str::Utf8Error),
+
+    #[error("`cargo fmt` failed with status {0}")]
+    CargoFmtFailed(ExitStatus),
+}
+
+fn formatted_paths(output: &[u8]) -> Result<Set<PathBuf>> {
+    let output = std::str::from_utf8(output)?;
+    output
+        .lines()
+        .filter_map(|line| line.strip_prefix("Formatting "))
+        .map(PathBuf::from)
+        .map(|path| path.canonicalize().map_err(Error::from))
+        .collect()
+}
--- crates/cargo-anodized/src/main.rs
+++ crates/cargo-anodized/src/main.rs
@@ -1,61 +1,22 @@
 use clap::Parser;
-use std::{
-    collections::BTreeSet as Set,
-    path::PathBuf,
-    process::{Command, ExitStatus},
-};
 
 use crate::cli::{Cli, Commands};
 
 mod cli;
+mod commands;
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Error> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Fmt => fmt()?,
+        Commands::Fmt => commands::fmt::fmt()?,
     }
 
     Ok(())
-}
-
-fn fmt() -> Result<()> {
-    let output = Command::new("cargo")
-        .args(["fmt", "--", "--verbose"])
-        .output()?;
-
-    let paths = formatted_paths(&output.stdout)?;
-    for path in paths {
-        println!("{}", path.display());
-    }
-
-    if !output.status.success() {
-        return Err(Error::CargoFmtFailed(output.status));
-    }
-
-    Ok(())
-}
-
-fn formatted_paths(output: &[u8]) -> Result<Set<PathBuf>> {
-    let output = std::str::from_utf8(output)?;
-    output
-        .lines()
-        .filter_map(|line| line.strip_prefix("Formatting "))
-        .map(PathBuf::from)
-        .map(|path| path.canonicalize().map_err(Error::from))
-        .collect()
 }
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
-    #[error("failed to run `cargo fmt`: {0}")]
-    Io(#[from] std::io::Error),
-
-    #[error("`cargo fmt` output was not valid UTF-8: {0}")]
-    Utf8(#[from] std::str::Utf8Error),
-
-    #[error("`cargo fmt` failed with status {0}")]
-    CargoFmtFailed(ExitStatus),
+    #[error("command `fmt` failed: {0}")]
+    Fmt(#[from] commands::fmt::Error),
 }
-
-type Result<T> = std::result::Result<T, Error>;
